d19a0975c0576046326784d9dc748990d760d4f3
Romain Charpentier <romain.charpentier10@gmail.com>
2024-02-25 22:07:05 +0100

    add password hashing

--- server/models.js
+++ server/models.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import argon2 from 'argon2';
 
 const authorizationSchema = new mongoose.Schema({
   token: String,
@@ -7,10 +8,28 @@ const authorizationSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, unique: true },
+  name: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
   token: String,
   cookie: String,
   expirationDate: Date
+});
+userSchema.virtual('id').get(() => this.name);
+userSchema.pre('findOneAndUpdate', async function (next) {
+  const updates = this.getUpdate();
+  const user = updates.$set || updates;
+
+  if (!user.password) {
+    return next();
+  }
+  try {
+    const hashedPassword = await argon2.hash(user.password);
+    user.password = hashedPassword;
+  } catch (err) {
+    console.error('Error while hashing password : ', err);
+  }
+  return next();
 });
 
 const Authorization = mongoose.model('Authorization', authorizationSchema);
--- server/server.js
+++ server/server.js
@@ -5,23 +5,44 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { getFiles, login } from './synology.api.js';
 import { Authorization, User } from './models.js';
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: '.env.local' });
+}
 
 const app = express();
 
 // MongoDB
-console.log('MONGO URL ::: ', process.env.MONGODB_URL);
 mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  // .then(() => {
-  //   console.log('Connected to MongoDB');
-  //   app.listen(5000, () => {
-  //     console.log('Server started on port 5000');
-  //   });
-  // })
-  .catch((err) => console.log(err));
+  .then(async () => {
+    console.error(
+      'Connection to MongoDB database successful : ',
+      process.env.MONGODB_URL
+    );
+    const adminData = {
+      name: process.env.ADMIN_NAME,
+      password: 'process.env.ADMIN_PASSWORD',
+      isAdmin: true
+    };
+    await User.findOneAndUpdate({ name: adminData.name }, adminData, {
+      upsert: true,
+      new: true
+    })
+      .then((adminUser) => {
+        console.log('Admin user updated successfully : ', adminUser);
+      })
+      .catch((err) => {
+        console.error('Error updating admin user : ', err);
+      });
+  })
+  .catch((err) => {
+    console.error('Error while connecting to Mongo dabatabse : ', err);
+  });
 
 // Middleware
 app.use(bodyParser.json());
@@ -40,6 +61,33 @@ app.get('/users', async (req, res) => {
   }
 });
 
+app.post('/log', async (req, res) => {
+  // Ici, vous pouvez implémenter la logique de connexion
+  // Récupérez les données d'identification de la requête
+  const { user, password } = req.body;
+
+  try {
+    // Vérifiez si l'utilisateur existe dans la base de données
+    const user = await User.findOne({ email });
+
+    // Si l'utilisateur n'existe pas ou si le mot de passe est incorrect
+    if (!user || !user.isValidPassword(password)) {
+      return res
+        .status(401)
+        .json({ message: 'Adresse e-mail ou mot de passe incorrect' });
+    }
+
+    // Si l'utilisateur existe et le mot de passe est correct, vous pouvez générer un jeton d'authentification
+    const token = generateAuthToken(user);
+
+    // Réponse avec le token
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erreur lors de la connexion' });
+  }
+});
+
 const getExpirationDate = (cookieString) => {
   const parts = cookieString.split(';');
 
@@ -55,24 +103,24 @@ const getExpirationDate = (cookieString) => {
   return expirationDate;
 };
 
-app.post('/login', async (req, res) => {
-  const response = await login(req.body.user, req.body.password);
-  res.set('Set-Cookie', response.headers['set-cookie']);
-  const user = {
-    name: req.body.user,
-    token: response.data.data.synotoken,
-    cookie: response.headers['set-cookie'].join(';'),
-    expirationDate: Math.min(
-      ...response.headers['set-cookie'].map((cookie) =>
-        getExpirationDate(cookie)
-      )
-    )
-  };
-  const newUser = await User.findOneAndReplace({ name: req.body.user }, user, {
-    upsert: true
-  });
-  res.status(200).json(newUser);
-});
+// app.post('/login', async (req, res) => {
+//   const response = await login(req.body.user, req.body.password);
+//   res.set('Set-Cookie', response.headers['set-cookie']);
+//   const user = {
+//     name: req.body.user,
+//     token: response.data.data.synotoken,
+//     cookie: response.headers['set-cookie'].join(';'),
+//     expirationDate: Math.min(
+//       ...response.headers['set-cookie'].map((cookie) =>
+//         getExpirationDate(cookie)
+//       )
+//     )
+//   };
+//   const newUser = await User.findOneAndReplace({ name: req.body.user }, user, {
+//     upsert: true
+//   });
+//   res.status(200).json(newUser);
+// });
 
 app.get('/files', async (req, res) => {
   const response = await getFiles(req.query.path, req.headers);
